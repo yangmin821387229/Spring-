09a77f09551e1b94b6ae91a793a3232bbf4545c2
yangmin821387229 <821387229@qq.com>
2019-01-13 23:49:24 +0800

    aop

---
+++ AOP/src/com/tutorialspoint/Logging.java
@@ -0,0 +1,22 @@
+package com.tutorialspoint;
+
+public class Logging {
+	public void beforeAdvice()
+	{
+		System.out.println("Going to setup student profile.");
+	}
+	
+	public void afterAdvice()
+	{
+		System.out.println("Student profile has been setup.");
+	}
+	
+	public void afterReturningAdvice(Object retVal)
+	{
+		System.out.println("Returning:"+retVal.toString());
+	}
+	
+	public void afterThrowingAdvice(IllegalArgumentException ex) {
+		System.out.println("There has been an exception:"+ex.toString());
+	}
+}
